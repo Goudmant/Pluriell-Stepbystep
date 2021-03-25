e6d7ec4716db733c0a30f1ffb83f8255592be421
Goudmant <yvangoudmant@gmail.com>
2021-03-25 13:23:00 +0100

    Init Stepbystep

---
+++ script.js
@@ -0,0 +1,48 @@
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const stepNumbers = document.querySelectorAll(".step-number");
+const stepProgression = document.querySelector("#stepProgression");
+
+let currentStep = 1;
+Update();
+
+stepNumbers.forEach((step, i) => {
+    step.onclick = () => {
+        currentStep = i + 1;
+        update();
+    }
+});
+
+prevBtn.onclick = () => {
+    currentStep--;
+    update();
+};
+
+nextBtn.onclick = () => {
+    currentStep++;
+    update();
+};
+
+function update() {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+    if (currentStep <= 1) {
+        currentStep = 1;
+        prevBtn.disabled = true;
+    } else if (currentStep >= stepNumbers.length) {
+        currentStep = stepNumbers.length;
+        nextBtn.disabled = true;
+    }
+
+    stepNumbers.forEach((step, i) => {
+        if (currentStep > i) {
+            step.classList.add("active");
+        } else {
+            step.classList.remove("active");
+        }
+    });
+
+    stepProgression.style.width = 
+        ((currentStep - 1) / (stepNumbers.length - 1 )) * 100 + "%";
+
+}
